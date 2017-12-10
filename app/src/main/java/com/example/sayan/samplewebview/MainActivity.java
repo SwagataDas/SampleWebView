1c61ef1be6e332f4e694caf1459778e84db43ccb
Sayan <msayanece@gmail.com>
2017-12-10 13:07:40 +0530

    init commit

---
+++ app/src/main/java/com/example/sayan/samplewebview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sayan.samplewebview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.web);
+
+        webView.loadUrl("https://www.google.co.in/");
+        webView.setWebViewClient(new myClient());
+    }
+
+    class myClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
